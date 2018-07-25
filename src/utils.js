62c40dc057e53691e2bf4473e4b89d9df96ad52f
Vinh Le <vinh.ninja@icloud.com>
2018-07-26 00:34:48 +0800

    Refine interactive cmds

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,5 @@
 import chalk from 'chalk'
+import packageInfo from '../package.json'
 
 const KEYPRESS_VALUE_CTRL_C = '\u0003'
 
@@ -32,18 +33,46 @@ const listenKeyPress = ({ onPressToggle, onPressNext, onClose }) => {
   })
 }
 
+const usage = chalk`
+  Shuffle play all SoundCloud tracks liked by an user via command line.
+
+  {magenta Usage}
+    ❯ {blue apolloud} {yellow <username>} {cyan <trackType>}
+
+    {cyan <trackType>} can be:
+      {white likes} (default) | {white all}
+
+  {magenta Examples}
+    ❯ {blue apolloud} {yellow vinlh}
+    ❯ {blue apolloud} {yellow vinlh} {cyan all}
+    ❯ {blue apolloud} {yellow vinlh} {cyan likes}
+`
+
 const validateCmdArgs = (userName, trackType) => {
+  if (userName === '-v') {
+    console.log(chalk`
+  {blue Apolloud} {magenta v${packageInfo.version}} by {yellow vinhlh}
+`)
+    return false
+  }
+
+  if (userName === '-h') {
+    console.log(usage)
+    return false
+  }
+
   if (!userName) {
-    console.error(chalk`SoundCloud user name is required!
-  Example: {yellow apolloud vinhlh}.
+    console.log(chalk`
+  {red SoundCloud user name is required!}
+${usage}
     `)
     return false
   }
 
   if (!validTypes.includes(trackType)) {
-    console.error(chalk`Invalid track type provided! Supported types: {yellow ${validTypes.join(
-      ', '
-    )}}
+    console.log(chalk`
+  {red Invalid track type provided!}
+${usage}
 `)
     return false
   }
